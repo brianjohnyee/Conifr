5172efb98df35dc34c288b419db27262ef346c5d
Sid Verma <sid@qvivr.com>
2018-02-06 08:30:51 -0800

    Initialization + Small UI Changes

---
+++ Conifr/Conifr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conifr
+//
+//  Created by Sid Verma on 2/6/18.
+//  Copyright © 2018 Sid Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Conifr/Pods/Target Support Files/Pods-Conifr/Pods-Conifr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Conifr : NSObject
+@end
+@implementation PodsDummy_Pods_Conifr
+@end
