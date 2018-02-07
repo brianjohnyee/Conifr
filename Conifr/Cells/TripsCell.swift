7f120cc24dd4c57034a2bfcef699ef7966dc2089
Sid Verma <siverma@ucsc.edu>
2018-02-06 17:38:38 -0800

    Added Design for trip cell

---
+++ Conifr/Cells/TripsCell.swift
@@ -0,0 +1,17 @@
+//
+//  TripsCell.swift
+//  
+//
+//  Created by Sid Verma on 2/6/18.
+//
+
+import UIKit
+
+class TripsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
