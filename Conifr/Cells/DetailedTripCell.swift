71f9d941e643512b44426523db38780a9a64171d
Kenji Mah <kemmah@ucsc.edu>
2018-02-19 14:42:17 -0800

    Semi combination

---
+++ Conifr/Cells/DetailedTripCell.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedTripStats.swift
+//  Conifr
+//
+//  Created by Kenji Maolo Mah on 2/17/18.
+//  Copyright © 2018 Sid Verma. All rights reserved.
+//
+
+import UIKit
+
+class DetailedTripCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
